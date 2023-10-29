4b1b8eb3c6fedea6eb97c7d86d07280864c8a9f4
chris-galey <cgaley54@gmail.com>
2023-10-29 18:49:16 -0400

    updated docker-compose-dev, docker-compose-prod. Added github actions for front and backend.

---
+++ run-compose-dev-ci.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# These environment variables are consumed by the docker-compose file.
+export SECRET_KEY=abc123
+export DEBUG=True
+export POSTGRES_DB=wines
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+
+docker-compose -f docker-compose.dev.yml up -d --build
+
+# make sure the postgres container is ready, then run migrations
+sleep 10
+
+# You have to change the container names
+docker exec samurAI-stocks_api_1  python /src/manage.py makemigrations 
+docker exec samurAI-stocks_api_1  python /src/manage.py migrate
