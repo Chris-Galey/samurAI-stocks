f397626f85696bf730cb666a41741b3851350ce9
jandro <jandrocodes@icloud.com>
2023-11-11 10:45:15 -0600

    linechart and some of watchlist

--- backend/stock_app/urls.py
+++ backend/stock_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import WatchlistView, WatchlistRetrieveDestroyView, MarketStatusView, StockSymbolsView, MarketHolidaysView, CompanyProfileView
+from .views import WatchlistView, WatchlistRetrieveDestroyView, MarketStatusView, StockSymbolsView, MarketHolidaysView, CompanyProfileView, get_finnhub_api_key
 
 urlpatterns =[
     path('', WatchlistView.as_view(), name='my_watchlist'),
@@ -13,4 +13,7 @@ urlpatterns =[
     path('marketholidays/', MarketHolidaysView.as_view(), name='get_market_holidays'),
 
     path('companyprofile/', CompanyProfileView.as_view(), name='get_company'),
+
+    path('get_finnhub_api_key/', get_finnhub_api_key, name='get_finnhub_api_key'),
+
 ]
--- backend/stock_app/views.py
+++ backend/stock_app/views.py
@@ -91,3 +91,8 @@ class CompanyProfileView(APIView):
         else:
             return JsonResponse({'error': 'Symbol parameter is required'})
 
+def get_finnhub_api_key(request):
+    api_key = os.getenv("FINNHUB_API_KEY")
+
+    if api_key:
+        return JsonResponse({'api_key': api_key})
