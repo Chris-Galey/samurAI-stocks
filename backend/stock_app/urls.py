2a7ac2f894c047673b3ed63df21c30c2ce13c181
jandro <jandrocodes@icloud.com>
2023-11-11 11:30:12 -0600

    Revert "linegraphs, and npm install issue"
    This reverts commit 9487a9e43f6326928f67c37c87437ac432b247bb.

--- backend/stock_app/urls.py
+++ backend/stock_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import WatchlistView, WatchlistRetrieveDestroyView, MarketStatusView, StockSymbolsView, MarketHolidaysView, CompanyProfileView, StockSymbolLookupView, get_finnhub_api_key
+from .views import WatchlistView, WatchlistRetrieveDestroyView, MarketStatusView, StockSymbolsView, MarketHolidaysView, CompanyProfileView, StockSymbolLookupView
 
 urlpatterns =[
     path('', WatchlistView.as_view(), name='my_watchlist'),
@@ -16,5 +16,4 @@ urlpatterns =[
 
     path('companyprofile/', CompanyProfileView.as_view(), name='get_company'),
 
-    path('get_finnhub_api_key/', get_finnhub_api_key, name='get_finnhub_api_key'),
 ]
